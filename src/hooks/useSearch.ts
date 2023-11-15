65f6a0927c0da45f66b39f31b2e46c21c6933e60
jbresky <joaquin_bresky@hotmail.com>
2023-11-15 19:19:00 -0300

    react-spinners

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+
+export default function useSearch() {
+    const [search, setSearch] = useState<string>('')
+
+    // useEffect(() => {
+
+    // }, [])
+  return 
+}
