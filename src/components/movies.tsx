60f8718fb68af50e7afcaeb1f82f28587c4f3bbf
jbresky <joaquin_bresky@hotmail.com>
2023-11-17 22:40:24 -0300

    sonner / spinners / html2canvas

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import { Movie } from "@/interface/schema"
 import CardMovie from "./card-movie"
 
-const ListOfMovies = ({ movies }: { movies: any }) => {
+interface IMovies {
+    movies: any,
+} 
+
+const ListOfMovies = ({ movies }: IMovies) => {
     return (
         <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-center">
             {
-                movies.map(({ id, title, poster_path }: Movie) => (
+                movies.map((item: any ) => (
                     <CardMovie
-                        key={id}
-                        id={id}
-                        title={title}
-                        poster_path={poster_path}
+                        item={item}
                     />
                 ))
             }
@@ -24,7 +24,7 @@ const EmptyState = () => {
     return <p className="text-grayth px-3">No movies to show</p>
 }
 
-export default function Movies({ movies }: { movies: any }) {
+export default function Movies({ movies, onClick }: any) {
     const hasMovies = movies?.length > 0
 
     return (
