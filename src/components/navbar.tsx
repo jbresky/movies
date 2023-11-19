c8617c3fa7c9b5c0b3c127c3e7b24fc342436e64
jbresky <joaquin_bresky@hotmail.com>
2023-11-19 14:35:10 -0300

    profile link hidden

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,21 +5,23 @@ import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
   const router = useRouter()
-  
+
   const handleLogout = async () => {
     await logOut()
     router.push('/login')
   }
 
   const { user, logOut } = UserAuth()
-  
+
   return (
     <header className="flex max-sm:flex-col max-sm:items-start items-center justify-between gap-2 px-10">
       <button className='text-2xl font-bold' onClick={() => router.push('/')}>
         Movies
       </button>
       <div className='text-[15px] font-semibold flex items-center gap-4'>
-        <button onClick={() => router.push('/account')}>Profile</button>
+        {user?.email && (
+          <button onClick={() => router.push('/account')}>Profile</button>
+        )}
         {user?.email
           ? (
             <button
@@ -30,8 +32,8 @@ const Navbar = () => {
           )
           : (
             <button
-            onClick={() => router.push('/signup')}
-            className='w-fit py-2 px-4 rounded-lg text-white hover:opacity-80'>
+              onClick={() => router.push('/signup')}
+              className='w-fit py-2 px-4 rounded-lg text-white hover:opacity-80'>
               Sign Up
             </button>
           )
