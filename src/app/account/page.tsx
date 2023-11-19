993fc36558202c46a54b73b5110ab7cfa4b72b08
jbresky <joaquin_bresky@hotmail.com>
2023-11-19 14:23:30 -0300

    ternary accessibility

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -5,6 +5,7 @@ import { UserAuth } from "@/context/auth-context";
 import { db } from "@/firebase";
 import { doc, onSnapshot } from "firebase/firestore";
 import html2canvas from "html2canvas";
+import { redirect, useRouter } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 
 const Account = () => {
@@ -12,6 +13,11 @@ const Account = () => {
     const { user } = UserAuth()
     const [movies, setMovies] = useState([])
     const [ranking, setRanking] = useState<any>([])
+    const router = useRouter()
+
+    if(!user){
+        redirect('/login')
+    }
 
     useEffect(() => {
         onSnapshot(doc(db, 'users', `${user?.email}`), doc => {
@@ -49,18 +55,25 @@ const Account = () => {
                     <h1 className="text-3xl">Profile</h1>
                 </div>
 
-                <section className="flex flex-col gap-6 pb-6">
-                    <div className="flex justify-between gap-10 text-grayth text-[15px] font-semibold">
+                <section className="flex flex-col gap-3 pb-6">
+                    <div className="flex justify-between text-grayth text-[15px] font-semibold">
                         <h2 className="text-2xl">My Rankings</h2>
-                        <button
-                            onClick={handleShare}
-                            className='text-xl border-indigo-800 border-2 rounded-lg p-2 hover:text-black hover:bg-indigo-600 transition duration-500'>
-                            Share ranking
-                        </button>
+
+                        {ranking?.length > 0 ? (
+                            <button
+                                onClick={handleShare}
+                                className='text-xl border-indigo-800 border-2 rounded-lg p-2 hover:text-black hover:bg-indigo-600 transition duration-500'>
+                                Share ranking
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => router.push('/create-ranking')}
+                                className="border-indigo-900 border-2 rounded-lg py-2 px-4 hover:brightness-150">Create your first ranking</button>
+                        )}
                     </div>
                     <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-start">
                         {
-                            ranking[0]?.selectedMovies.map((item: any) => (
+                            ranking && ranking[0]?.selectedMovies.map((item: any) => (
                                 <div
                                     className="max-xl:w-[250px] xl:w-[300px] flex flex-col gap-2 text-ellipsis overflow-hidden whitespace-nowrap" key={item.id}>
                                     <CustomContainer
@@ -74,17 +87,17 @@ const Account = () => {
                         }
                     </div>
                 </section>
-                <section>
-                    <h2 className="text-2xl pb-2">Favorites</h2>
-                    {movies.length === 0 && (
+                <section className="flex flex-col gap-3">
+                    <h2 className="text-2xl pb-2 text-grayth font-semibold">Favorites</h2>
+                    {movies && movies.length === 0 && (
                         <p className="text-grayth">You don&apos;t have any favorites yet</p>
                     )}
-                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-center">
+                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-start">
                         {
                             movies?.map((item: any) => (
                                 <div className="max-xl:w-[250px] xl:w-[300px] flex flex-col gap-2 text-ellipsis overflow-hidden whitespace-nowrap" key={item.id}>
                                     <CustomContainer
-                                        classname="bg-red-600 text-xl hover:text-gray-400 transition duration-200 cursor-pointer absolute top-3 right-3"
+                                        classname='text-red-700 text-xl hover:opacity-80 transition duration-200 cursor-pointer absolute top-3 right-3'
                                         item={item}
                                         isRank={false}
                                     />
