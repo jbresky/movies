54ec1371d611e8fe9fbacc699de4c54ab2529856
jbresky <joaquin_bresky@hotmail.com>
2023-11-22 17:48:29 -0300

    form validation

--- src/components/modals/login-modal.tsx
+++ src/components/modals/login-modal.tsx
@@ -3,28 +3,46 @@
 import { UserAuth } from "@/context/auth-context";
 import useLoginModal from "@/hooks/use-login-modal";
 import useRegisterModal from "@/hooks/use-register-modal";
-import { useCallback, useState } from "react";
+import { FormEvent, useCallback, useState } from "react";
 import Modal from "./modal";
 import { FcGoogle } from 'react-icons/fc'
 import { toast } from 'sonner'
+import { validateEmail } from "@/utils/validateEmail";
 
 const LoginModal = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState({
+        value: "",
+        isTouched: false
+    })
+    const [password, setPassword] = useState({
+        value: "",
+        isTouched: false
+    })
+    const [loading, setLoading] = useState(false)
+
     const { logIn, googleSignIn } = UserAuth()
 
     const loginModal = useLoginModal()
     const registerModal = useRegisterModal()
 
-    const signInWithCredentials = async () => {
+    const isFormValid = () => {
+        return validateEmail(email.value) && password.value.length >= 5
+    }
+
+    const signInWithCredentials = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
         try {
-            await logIn(email, password)
+            setLoading(true)
+            await logIn(email.value, password.value)
             loginModal.onClose()
-            setEmail('')
-            setPassword('')
+            setEmail({ value: "", isTouched: false })
+            setPassword({ value: "", isTouched: false })
             toast.success('Logged in')
         } catch (error) {
             console.log(error);
+            toast.error('Invalid credentials')
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -45,28 +63,49 @@ const LoginModal = () => {
 
 
     const body = (
-        <div className="flex flex-col items-center md:m-auto gap-12 w-full">
-            <div className="flex flex-col gap-4 w-full mt-4">
-                <input
-                    className="rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                    placeholder="Email"
-                    onChange={((e: any) => setEmail(e.target.value))}
-                    value={email}
-                    name='email'
-                />
-                <input
-                    type="password"
-                    className="rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                    placeholder="Password"
-                    onChange={((e: any) => setPassword(e.target.value))}
-                    value={password}
-                    name='password'
-                />
-            </div>
+        <div className="flex flex-col items-center md:m-auto gap-6 w-full">
+            <form onSubmit={signInWithCredentials} className="w-full">
+                <div className="flex flex-col gap-6 w-full mt-4">
+                    <div>
+                        <input
+                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                            placeholder="Email"
+                            onChange={((e: any) => setEmail({ ...email, value: e.target.value }))}
+                            onBlur={() => { 
+                                setEmail({ ...email, isTouched: true }); 
+                              }} 
+                            value={email.value}
+                            name='email'
+                        />
+                        {email.isTouched && !validateEmail(email.value) ? <p className="text-sm text-red-500 pt-2 pl-1">
+                            Please provide a valid email
+                        </p> : null}
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                            placeholder="Password"
+                            onChange={((e: any) => setPassword({ ...password, value: e.target.value }))}
+                            onBlur={() => { 
+                                setPassword({ ...password, isTouched: true }); 
+                              }} 
+                            value={password.value}
+                            name='password'
+                        />
+                        {password.isTouched && password.value.length < 5 ? <p className="text-sm text-red-500 pt-2 pl-1">
+                            Password must contain at least 5 characters
+                        </p> : null}
+                    </div>
+                    <button
+                        disabled={!isFormValid()}
+                        type="submit"
+                        className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg focus:brightness-200">
+                        Continue
+                    </button>
+                </div>
+            </form>
             <div className="flex flex-col gap-4 w-full items-center">
-                <button
-                    onClick={signInWithCredentials}
-                    className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg hover:brightness-110 focus:brightness-200">Continue</button>
                 <p>or</p>
                 <div
                     onClick={signInWithGoogle}
@@ -85,6 +124,7 @@ const LoginModal = () => {
             onClose={loginModal.onClose}
             title="Log in"
             body={body}
+            loading={loading}
         />
     );
 }
--- src/components/modals/register-modal.tsx
+++ src/components/modals/register-modal.tsx
@@ -7,24 +7,51 @@ import { FormEvent, useCallback, useRef, useState } from "react"
 import Modal from "./modal"
 import { FcGoogle } from 'react-icons/fc'
 import { toast } from 'sonner'
+import { validateEmail } from "@/utils/validateEmail"
 
 const RegisterModal = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const { signUp } = UserAuth()
+    const [email, setEmail] = useState({
+        value: "",
+        isTouched: false
+    })
+    const [password, setPassword] = useState({
+        value: "",
+        isTouched: false
+    })
+    const [loading, setLoading] = useState(false)
+
+
+    const { signUp, googleSignIn } = UserAuth()
 
     const formRef = useRef<HTMLFormElement>(null)
 
     const loginModal = useLoginModal()
     const registerModal = useRegisterModal()
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    const isFormValid = () => {
+        return validateEmail(email.value) && password.value.length >= 5
+    }
+
+    const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
-            await signUp(email, password)
+            setLoading(true)
+            await signUp(email.value, password.value)
             formRef.current?.reset()
             registerModal.onClose()
             toast.success('Welcome!')
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const signInWithGoogle = async () => {
+        try {
+            await googleSignIn()
+            loginModal.onClose()
+            toast.success('Logged in')
         } catch (error) {
             console.log(error);
         }
@@ -36,37 +63,59 @@ const RegisterModal = () => {
     }, [loginModal, registerModal])
 
     const body = (
-        <form
-            ref={formRef}
-            className="flex flex-col items-center md:m-auto gap-12 w-full"
-            onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-4 w-full mt-4">
-                <input
-                    className="rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                    placeholder="Email"
-                    onChange={((e: any) => setEmail(e.target.value))}
-                    value={email}
-                    name='email'
-                />
-                <input
-                    type="password"
-                    className="rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                    placeholder="Password"
-                    onChange={((e: any) => setPassword(e.target.value))}
-                    value={password}
-                    name='password'
-                />
-            </div>
+        <div className="flex flex-col items-center md:m-auto gap-6 w-full">
+            <form onSubmit={handleSignUp} className="w-full">
+                <div className="flex flex-col gap-6 w-full mt-4">
+                    <div>
+                        <input
+                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                            placeholder="Email"
+                            onChange={((e: any) => setEmail({ ...email, value: e.target.value }))}
+                            onBlur={() => { 
+                                setEmail({ ...email, isTouched: true }); 
+                              }} 
+                            value={email.value}
+                            name='email'
+                        />
+                        {email.isTouched && !validateEmail(email.value) ? <p className="text-sm text-red-500 pt-2 pl-1">
+                            Please provide a valid email
+                        </p> : null}
+                    </div>
+                    <div>
+                        <input
+                            type="password"
+                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                            placeholder="Password"
+                            onChange={((e: any) => setPassword({ ...password, value: e.target.value }))}
+                            onBlur={() => { 
+                                setPassword({ ...password, isTouched: true }); 
+                              }} 
+                            value={password.value}
+                            name='password'
+                        />
+                        {password.isTouched && password.value.length < 5 ? <p className="text-sm text-red-500 pt-2 pl-1">
+                            Password must contain at least 5 characters
+                        </p> : null}
+                    </div>
+                    <button
+                        disabled={!isFormValid()}
+                        type="submit"
+                        className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg focus:brightness-200">
+                        Continue
+                    </button>
+                </div>
+            </form>
             <div className="flex flex-col gap-4 w-full items-center">
-                <button type="submit" className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg hover:brightness-110 focus:brightness-200">Continue</button>
                 <p>or</p>
-                <button className="w-full font-semibold bg-white text-gray-800 py-2 rounded-lg relative flex items-center justify-center hover:opacity-90">
+                <div
+                    onClick={signInWithGoogle}
+                    className="w-full font-semibold bg-white text-gray-800 py-2 rounded-lg relative flex items-center justify-center cursor-pointer hover:opacity-90">
                     <FcGoogle className="absolute left-4 top-3" />
                     Continue with Google
-                </button>
+                </div>
                 <p className="text-sm text-grayth self-start">Already have an account? <span className="hover:underline cursor-pointer" onClick={onToggle}>Login</span></p>
             </div>
-        </form>
+        </div>
     )
     return (
         <Modal
@@ -74,6 +123,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             title="Sign Up"
             body={body}
+            loading={loading}
         />
     );
 }
