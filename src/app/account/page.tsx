0f7126af0ae1179ecf67627bfa67bd25202e9a28
jbresky <joaquin_bresky@hotmail.com>
2023-11-22 19:36:50 -0300

    center container on mobile

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -4,15 +4,16 @@ import CustomContainer from "@/components/custom-container";
 import { UserAuth } from "@/context/auth-context";
 import { db } from "@/firebase";
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
-import html2canvas from "html2canvas";
+// import html2canvas from "html2canvas";
 import { redirect, useRouter } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 
 const Account = () => {
-    const componentRef = useRef<any>(null)
+    // const componentRef = useRef<any>(null)
     const { user } = UserAuth()
-    const [movies, setMovies] = useState([])
+    const [favorites, setFavorites] = useState([])
     const [ranking, setRanking] = useState<any>([])
+    
     const router = useRouter()
 
     if (!user) {
@@ -21,38 +22,38 @@ const Account = () => {
 
     useEffect(() => {
         onSnapshot(doc(db, 'users', `${user?.email}`), doc => {
-            setMovies(doc.data()?.savedMovies)
+            setFavorites(doc.data()?.savedMovies)
             setRanking(doc.data()?.ranking)
         })
     }, [user?.email]);
 
-    const handleShare = async () => {
-        try {
-            const canvas = await html2canvas(componentRef?.current)
-            const dataURL = canvas.toDataURL()
+    // const handleShare = async () => {
+    //     try {
+    //         const canvas = await html2canvas(componentRef?.current)
+    //         const dataURL = canvas.toDataURL()
 
-            const file = new File([dataURL], 'ranking_image.png', { type: 'image/png' })
+    //         const file = new File([dataURL], 'ranking_image.png', { type: 'image/png' })
 
-            const shareableMsg = `This is my top ${ranking.length} movie ranking!`
-            const shareData = {
-                title: 'Movie Ranking',
-                text: shareableMsg,
-                files: [file]
-            }
+    //         const shareableMsg = `This is my top ${ranking.length} movie ranking!`
+    //         const shareData = {
+    //             title: 'Movie Ranking',
+    //             text: shareableMsg,
+    //             files: [file]
+    //         }
 
-            await navigator.share(shareData)
-            console.log('Shared successfully');
+    //         await navigator.share(shareData)
+    //         console.log('Shared successfully');
 
-        } catch (error) {
-            console.error('Error sharing: ', error)
-        }
-    }
+    //     } catch (error) {
+    //         console.error('Error sharing: ', error)
+    //     }
+    // }
 
     const movieRef = doc(db, 'users', `${user?.email}`)
 
     const removeFromFavorites = async (movieId: string | number) => {
         try {
-            const result = movies.filter((item: any) => item.id !== movieId)
+            const result = favorites.filter((item: any) => item.id !== movieId)
             await updateDoc(movieRef, {
                 savedMovies: result
             })
@@ -74,7 +75,7 @@ const Account = () => {
 
                         {ranking?.length > 0 ? (
                             <button
-                                onClick={handleShare}
+                                // onClick={handleShare}
                                 className='text-xl border-indigo-800 border-2 rounded-lg p-2 hover:text-black hover:bg-indigo-600 transition duration-500'>
                                 Share ranking
                             </button>
@@ -84,13 +85,13 @@ const Account = () => {
                                 className="w-full xsm:w-fit sm:w-1/2 lg:w-1/4 border-indigo-900 border-2 rounded-lg py-2 px-4 hover:brightness-150">Create your first ranking</button>
                         )}
                     </div>
-                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-start">
+                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-center sm:justify-items-start">
                         {
                             ranking && ranking[0]?.selectedMovies.map((item: any) => (
                                 <div
                                     className="max-xl:w-[250px] xl:w-[300px] flex flex-col gap-2 text-ellipsis overflow-hidden whitespace-nowrap" key={item.id}>
                                     <CustomContainer
-                                        ref={componentRef}
+                                        // ref={componentRef}
                                         item={item}
                                         isRank={true}
                                         classname="text-grayth/60 text-2xl transition duration-200 cursor-pointer absolute top-3 right-3"
@@ -102,12 +103,12 @@ const Account = () => {
                 </section>
                 <section className="flex flex-col gap-6">
                     <h2 className="text-xl md:text-2xl text-grayth font-semibold">Favorites</h2>
-                    {movies && movies.length === 0 && (
+                    {favorites && favorites.length === 0 && (
                         <p className="text-grayth">You don&apos;t have any favorites yet</p>
                     )}
-                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-start">
+                    <div className="grid grid-cols-xl xl:grid-cols-2xl gap-4 justify-items-center sm:justify-items-start">
                         {
-                            movies?.map((item: any) => (
+                            favorites?.map((item: any) => (
                                 <div className="max-xl:w-[250px] xl:w-[300px] flex flex-col gap-2 text-ellipsis overflow-hidden whitespace-nowrap" key={item.id}>
                                     <CustomContainer
                                         classname='text-red-700 text-xl hover:opacity-80 transition duration-200 cursor-pointer absolute top-3 right-3'
