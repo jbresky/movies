6d4456742d090222ff5220f79f8aa4eeeca54360
jbresky <joaquin_bresky@hotmail.com>
2023-11-24 22:12:18 -0300

    modal styling

--- src/components/modals/login-modal.tsx
+++ src/components/modals/login-modal.tsx
@@ -63,46 +63,44 @@ const LoginModal = () => {
 
     const body = (
         <div className="flex flex-col items-center md:m-auto gap-6 w-full">
-                <div className="flex flex-col gap-6 w-full mt-4">
-                    <div>
-                        <input
-                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                            placeholder="Email"
-                            onChange={((e: any) => setEmail({ ...email, value: e.target.value }))}
-                            onBlur={() => {
-                                setEmail({ ...email, isTouched: true });
-                            }}
-                            value={email.value}
-                            name='email'
-                        />
-                        {email.isTouched && !validateEmail(email.value) ? <p className="text-sm text-red-500 pt-2 pl-1">
-                            Please provide a valid email
-                        </p> : null}
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
-                            placeholder="Password"
-                            onChange={((e: any) => setPassword({ ...password, value: e.target.value }))}
-                            onBlur={() => {
-                                setPassword({ ...password, isTouched: true });
-                            }}
-                            value={password.value}
-                            name='password'
-                        />
-                        {password.isTouched && password.value.length < 5 ? <p className="text-sm text-red-500 pt-2 pl-1">
-                            Password must contain at least 5 characters
-                        </p> : null}
-                    </div>
-                    <button
-                        disabled={!isFormValid()}
-                        onClick={signInWithCredentials}
-                        className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg focus:brightness-200">
-                        Continue
-                    </button>
+            {/* <div className="flex flex-col gap-6 w-full mt-4"> */}
+                <div className="w-full">
+                    <input
+                        className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                        placeholder="Email"
+                        onChange={((e: any) => setEmail({ ...email, value: e.target.value }))}
+                        onBlur={() => {
+                            setEmail({ ...email, isTouched: true });
+                        }}
+                        value={email.value}
+                        name='email'
+                    />
+                    {email.isTouched && !validateEmail(email.value) ? <p className="text-sm text-red-500 pt-2 pl-1">
+                        Please provide a valid email
+                    </p> : null}
                 </div>
-            <div className="flex flex-col gap-4 w-full items-center">
+                <div className="w-full">
+                    <input
+                        type="password"
+                        className="w-full rounded-lg border-2 border-indigo-900 bg-transparent outline-none focus:brightness-200 p-4"
+                        placeholder="Password"
+                        onChange={((e: any) => setPassword({ ...password, value: e.target.value }))}
+                        onBlur={() => {
+                            setPassword({ ...password, isTouched: true });
+                        }}
+                        value={password.value}
+                        name='password'
+                    />
+                    {password.isTouched && password.value.length < 5 ? <p className="text-sm text-red-500 pt-2 pl-1">
+                        Password must contain at least 5 characters
+                    </p> : null}
+                </div>
+                <button
+                    disabled={!isFormValid()}
+                    onClick={signInWithCredentials}
+                    className="w-full font-semibold self-center bg-indigo-900 text-gray-300 py-2 rounded-lg focus:brightness-200">
+                    Continue
+                </button>
                 <p>or</p>
                 <div
                     onClick={signInWithGoogle}
@@ -111,7 +109,7 @@ const LoginModal = () => {
                     Continue with Google
                 </div>
                 <p className="text-sm text-grayth self-start">First time using Movies? <span className="hover:underline cursor-pointer" onClick={onToggle}>Sign Up</span></p>
-            </div>
+            {/* </div> */}
         </div>
     )
 
