312ed6d76703a533f5883ef044fe3e27d9a71ed9
jbresky <joaquin_bresky@hotmail.com>
2023-11-30 22:24:35 -0300

    handling routing with dropdown - button styling

--- src/components/search-movies.tsx
+++ src/components/search-movies.tsx
@@ -1,7 +1,22 @@
 'use client'
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { UserAuth } from "@/context/auth-context";
+import useLoginModal from "@/hooks/use-login-modal";
+import useRegisterModal from "@/hooks/use-register-modal";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent } from 'react';
 import { FaSearch } from 'react-icons/fa'
+import { RxHamburgerMenu } from "react-icons/rx";
+import LoginModal from "./modals/login-modal";
+import RegisterModal from "./modals/register-modal";
 interface SearchProps {
   submitSearch: (e: FormEvent<HTMLFormElement>) => void,
   title: string,
@@ -13,30 +28,78 @@ interface SearchProps {
 
 const Search = ({ submitSearch, title, changeSearch, getTopRanked, hidden, children }: SearchProps) => {
 
+  const { user, logOut } = UserAuth()
+  const router = useRouter()
+
+  const handleLogout = () => {
+    logOut()
+    router.push('/')
+  }
+
+  const loginModal = useLoginModal()
+  const registerModal = useRegisterModal()
+
   return (
-    <div className='flex items-center justify-between gap-4 sm:px-4 py-4'>
-      {!hidden && (
-        <div className={`justify-between gap-4 text-grayth text-[15px] font-semibold hidden sm:flex`}>
-          <h3 className="hover:text-indigo-400 transition duration-200 cursor-pointer" onClick={getTopRanked}>Top ranked</h3>
-          {children ? children : ''}
+    <>
+      <LoginModal />
+      <RegisterModal />
+      <div className='sm:flex gap-6 sm:px-4 py-4'>
+        <div className='flex items-center justify-between gap-4 mb-4'>
+          <h1 className='sm:hidden font-bold font-mono text-2xl'>M</h1>
+          <form onSubmit={submitSearch} className="w-full sm:w-[300px]">
+            <div
+              className="flex items-center gap-3 text-sm text-grayth px-2 py-1 rounded-lg border-2 border-gray-600 hover:border-indigo-900 transition duration-200">
+              <input
+                value={title}
+                name='title'
+                onChange={changeSearch}
+                placeholder="Search movie..."
+                className="bg-transparent outline-none w-full"
+              />
+              <button type='submit' className='border-none outline-none focus:text-white'>
+                <FaSearch className='border-none' />
+              </button>
+            </div>
+          </form>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <RxHamburgerMenu className='sm:hidden text-xl' />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-[#060d17] text-grayth w-[150px] mt-2 mr-2">
+              {user && user.email ? (
+                <>
+                  <DropdownMenuLabel>
+                    <Link href='/account'>
+                      Profile
+                    </Link>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link href='/create-ranking'>
+                      Create ranking
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={handleLogout}>
+                    Logout
+                  </DropdownMenuItem>
+                </>
+              ) : (
+                <>
+                  <DropdownMenuItem onClick={loginModal.onOpen}>Log in</DropdownMenuItem>
+                  <DropdownMenuItem onClick={registerModal.onOpen}>Sign up</DropdownMenuItem>
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-      )}
-      <form onSubmit={submitSearch} className="w-full xsm:w-[300px]">
-        <div
-          className="flex items-center gap-3 text-sm text-grayth p-2 rounded-lg border-2 border-gray-600 hover:border-indigo-900 transition duration-200">
-          <input
-            value={title}
-            name='title'
-            onChange={changeSearch}
-            placeholder="Search movie..."
-            className="bg-transparent outline-none w-full"
-          />
-          <button type='submit' className='border-none outline-none focus:text-white'>
-            <FaSearch className='border-none' />
-          </button>
-        </div>
-      </form>
-    </div>
+        {!hidden && (
+          <div className='justify-between gap-4 text-grayth text-[15px] font-semibold overflow-x-auto flex whitespace-nowrap scroll-smooth'>
+            <h3 className="hover:text-indigo-40 transition duration-200 cursor-pointer text-sm max-sm:text-black max-sm:py-1 px-2 rounded-lg max-sm:bg-gradient-to-t from-indigo-900 to-pink-900 brightness-200" onClick={getTopRanked}>Top ranked</h3>
+            {children ? children : ''}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
