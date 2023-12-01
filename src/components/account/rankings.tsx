e4b3e31052227865d286e3aa3cde94552fd44e51
jbresky <joaquin_bresky@hotmail.com>
2023-11-30 22:22:50 -0300

    dropdown menu: update & delete

--- src/components/account/rankings.tsx
+++ src/components/account/rankings.tsx
@@ -1,7 +1,10 @@
 'use client'
 
 import { Movie } from "@/interface/movie-interface";
+import { TiArrowSortedDown } from "react-icons/ti";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import CustomContainer from "../custom-container";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "../ui/dropdown-menu";
 import AccountHeader from "./header";
 
 interface RankingsProps {
@@ -20,10 +23,11 @@ const Rankings = ({ ranking, removeRanking }: RankingsProps) => {
                     paragraph="You don't have rankings"
                 />
                 {ranking?.length > 0 && (
-                    <button
-                        className='text-lg border-indigo-800 border-2 rounded-lg px-4 py-1 hover:text-black hover:bg-indigo-600 transition duration-500'>
-                        Share ranking
-                    </button>
+                    // <button
+                    //     className='text-lg border-indigo-800 border-2 rounded-lg px-4 py-1 hover:text-black hover:bg-indigo-600 transition duration-500'>
+                    //     Share ranking
+                    // </button>
+                    ''
                 )}
             </div>
 
@@ -32,11 +36,25 @@ const Rankings = ({ ranking, removeRanking }: RankingsProps) => {
                     <>
                         <div className="flex justify-between items-center">
                             <h2 key={index} className="text-indigo-400 text-lg font-semibold">{rank.name}</h2>
-                            <button
-                                onClick={() => removeRanking(ranking.indexOf(rank))}
-                                className="hover:opacity-90 text-grayth cursor-pointer border-none outline-none">Delete</button>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="">
+                                    <TiArrowSortedDown className="text-indigo-600 text-2xl cursor-pointer" />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="bg-[#060d17] text-white mr-5">
+                                    <DropdownMenuItem>
+                                        <button>Update</button>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <button
+                                            onClick={() => removeRanking(ranking.indexOf(rank))}
+                                            className="hover:opacity-90 cursor-pointer border-none outline-none">
+                                            Delete
+                                        </button>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
-                        
+
                         <div className="grid grid-cols-sm md:grid-cols-md lg:grid-cols-lg xl:grid-cols-xl gap-4 mb-4">
                             {rank.movies && rank.movies.map((item: any) => (
                                 <div
