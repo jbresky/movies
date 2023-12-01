5628beef63a90c4b9627c3477da492bd5904549c
jbresky <joaquin_bresky@hotmail.com>
2023-11-30 22:26:47 -0300

    key - iterator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
           {
             movies.map((item: Movie) => (
               loading ? (
-                <div className="space-y-3">
+                <div key={item.id} className="space-y-3">
                   <Skeleton className="h-[375px] w-[170px] md:w-[200px] lg:w-[250px] xl:w-[300px]" />
                   <Skeleton className="h-2 w-[120px] md:w-[150px] lg:w-[200px] xl:w-[250px]" />
                 </div>
