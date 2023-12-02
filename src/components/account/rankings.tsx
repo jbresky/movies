22cf464f17dbf880f2df4278226669305d22cc1b
jbresky <joaquin_bresky@hotmail.com>
2023-12-02 15:51:34 -0300

    dropdown menu: search / delete

--- src/components/account/rankings.tsx
+++ src/components/account/rankings.tsx
@@ -1,69 +1,61 @@
 'use client'
 
+import AccountHeader from "./header";
+import CustomContainer from "../custom-container";
 import { Movie } from "@/interface/movie-interface";
 import { TiArrowSortedDown } from "react-icons/ti";
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
-import CustomContainer from "../custom-container";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from "../ui/dropdown-menu";
-import AccountHeader from "./header";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 interface RankingsProps {
-    ranking: Movie[]
+    rankings: Movie[]
     removeRanking: (ranking: number) => void
 }
 
-const Rankings = ({ ranking, removeRanking }: RankingsProps) => {
+const Rankings = ({ rankings, removeRanking }: RankingsProps) => {
 
     return (
         <section className="flex flex-col gap-4">
             <div className="flex flex-col xsm:flex-row gap-2 justify-between xsm:items-end text-[15px]">
                 <AccountHeader
-                    data={ranking}
+                    data={rankings}
                     title="Rankings"
                     paragraph="You don't have rankings"
                 />
-                {ranking?.length > 0 && (
-                    // <button
-                    //     className='text-lg border-indigo-800 border-2 rounded-lg px-4 py-1 hover:text-black hover:bg-indigo-600 transition duration-500'>
-                    //     Share ranking
-                    // </button>
-                    ''
-                )}
             </div>
 
             {
-                ranking && ranking.map((rank: any, index: number) => (
+                rankings && rankings.map((rank: any, index: number) => (
                     <>
-                        <div className="flex justify-between items-center">
-                            <h2 key={index} className="text-indigo-400 text-lg font-semibold">{rank.name}</h2>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger className="">
-                                    <TiArrowSortedDown className="text-indigo-600 text-2xl cursor-pointer" />
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent className="bg-[#060d17] text-white mr-5">
-                                    <DropdownMenuItem>
-                                        <button>Update</button>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <button
-                                            onClick={() => removeRanking(ranking.indexOf(rank))}
-                                            className="hover:opacity-90 cursor-pointer border-none outline-none">
-                                            Delete
-                                        </button>
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+                        <div className="flex justify-between items-center" key={index}>
+                            <h2 className="text-indigo-400 text-lg sm:text-xl font-semibold">{rank.name}</h2>
+                            <>                                <DropdownMenu>
+                                    <DropdownMenuTrigger className="">
+                                        <TiArrowSortedDown className="text-indigo-500 text-2xl cursor-pointer" />
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="bg-[#060d17] font-semibold text-indigo-300 mr-5">
+                                        <DropdownMenuItem>
+                                            <button className="w-full text-left hover:opacity-90">Share</button>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <button
+                                                onClick={() => removeRanking(rankings.indexOf(rank))}
+                                                className="w-full text-left hover:opacity-90 border-none outline-none">
+                                                Delete
+                                            </button>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </>
                         </div>
 
                         <div className="grid grid-cols-sm md:grid-cols-md lg:grid-cols-lg xl:grid-cols-xl gap-4 mb-4">
                             {rank.movies && rank.movies.map((item: any) => (
-                                <div
-                                    className="" key={item.id}>
-                                    <CustomContainer
-                                        item={item}
-                                        isRank={true}
-                                        classname="text-grayth/60 text-2xl transition duration-200 cursor-pointer absolute top-3 right-3"
-                                    />
+                                <div key={item.id}>
+                                        <CustomContainer
+                                            item={item}
+                                            isRank={true}
+                                            classname="text-grayth/60 text-2xl transition duration-200 cursor-pointer absolute top-3 right-3"
+                                        />
                                 </div>
                             ))}
                         </div>
