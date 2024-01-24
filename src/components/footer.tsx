29ba7944ce860ce961f928426c35da26d6441fbb
jbresky <joaquin_bresky@hotmail.com>
2024-01-24 18:01:17 -0300

    Footer - not using yet

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return <footer className="flex justify-between items-center p-2 mt-8 text-[12.5px] tracking-wide text-grayth">
+        <p>Created by</p>
+        <span className="hover:text-[#665ddc] transition duration-200 cursor-pointer">@jbresky</span>
+    </footer>
+}
+
+export default Footer;
