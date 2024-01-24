5b1fdded7e2bb021420cd7e88735250b003e6e51
jbresky <joaquin_bresky@hotmail.com>
2024-01-24 18:00:42 -0300

    moving modals to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import LoginModal from '@/components/modals/login-modal'
+import RegisterModal from '@/components/modals/register-modal'
 import Navbar from '@/components/navbar'
 import { AuthContextProvider } from '@/context/auth-context'
 import type { Metadata } from 'next'
@@ -20,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <AuthContextProvider>
         <body className={inter.className}>
-          <div className="flex min-h-screen max-w-[1700px] m-auto flex-col py-2 px-4 lg:px-10">
+          <LoginModal />
+          <RegisterModal />
+          <div className="flex flex-col max-w-[1700px] py-2 px-4 lg:px-10 min-h-screen">
             <Navbar />
             {children}
           </div>
