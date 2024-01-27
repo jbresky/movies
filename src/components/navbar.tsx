3bdf6798e351b8ec3999b250bed468a7f40ce979
jbresky <joaquin_bresky@hotmail.com>
2024-01-27 15:12:36 -0300

    use of dropdown

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,16 +2,14 @@
 
 import useLoginModal from '@/hooks/use-login-modal';
 import useRegisterModal from '@/hooks/use-register-modal';
-import Image from 'next/image';
 import { UserAuth } from '@/context/auth-context';
-import { usePathname, useRouter } from 'next/navigation';
-import { FaCircleUser } from "react-icons/fa6";
 import Link from 'next/link';
+import Dropdown from './dropdown';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
-  const router = useRouter()
-  const path = usePathname()
 
+  const router = useRouter()
   const { user, logOut } = UserAuth()
 
   const handleLogout = () => {
@@ -28,49 +26,31 @@ const Navbar = () => {
     <>
       <header className="sm:flex items-center justify-between gap-2 hidden sm:p-4">
         <Link href='/' className='text-2xl font-bold font-mono py-2'>
-            Movieees
+          Movieees
         </Link>
-        <div className='text-[15px] font-semibold items-center gap-6 hidden sm:flex'>
-          {user?.email && path !== '/create-ranking' ? <Link href='/create-ranking' className='hover:opacity-80'>Create ranking</Link> : null}
-          {user?.email && path !== '/account' ? <Link href='/account' className='hover:opacity-80'>Profile</Link> : null}
-          {user && user?.email
-            ? (
+
+        {user && user?.email
+          ? (
+            <Dropdown
+              user={user}
+              handleLogout={handleLogout}
+            />
+          )
+          : (
+            <div className='flex font-sans font-medium items-center gap-3'>
               <button
-                onClick={handleLogout}
-                className='w-fit text-white hover:opacity-80'>
-                Log out
+                onClick={registerModal.onOpen}
+                className='w-fit rounded-lg py-2 px-4 text-white hover:opacity-80'>
+                Sign Up
               </button>
-            )
-            : (
-              <div className='flex items-center gap-3'>
-                <button
-                  onClick={registerModal.onOpen}
-                  className='w-fit rounded-lg py-2 px-4 text-white hover:opacity-80'>
-                  Sign Up
-                </button>
-                <button
-                  onClick={loginModal.onOpen}
-                  className='w-fit bg-slate-800 py-2 px-4 rounded-lg text-white hover:opacity-80'>
-                  Log in
-                </button>
-              </div>
-            )
-          }
-        </div>
-        {user && user.photoURL && (
-          <Image
-            className='rounded-full sm:hidden'
-            width={40}
-            height={40}
-            alt={user.email}
-            src={user.photoURL}
-          />
-        )}
-        {user && !user.photoURL && (
-          <FaCircleUser
-            className='rounded-full sm:hidden text-2xl bg-indigo-900'
-          />
-        )}
+              <button
+                onClick={loginModal.onOpen}
+                className='w-fit bg-slate-800 py-2 px-4 rounded-lg text-white hover:opacity-80'>
+                Log in
+              </button>
+            </div>
+          )
+        }
       </header>
     </>
   );
