55e691a5c418d242f51350ac826935320fe75c63
jbresky <joaquin_bresky@hotmail.com>
2024-01-27 15:14:48 -0300

    change in profile page: more routes

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import Rankings from "@/components/account/rankings";
-import Favorites from "@/components/account/favorites";
 import useSavedMovies from "@/hooks/use-saved-movies";
 import { UserAuth } from "@/context/auth-context";
 import { redirect } from "next/navigation";
+import { FaRegFolder } from "react-icons/fa";
 import Loader from "@/components/loader";
 import Search from "@/components/search-movies";
+import Link from "next/link";
+
+const NewFeature = () => {
+  return (
+    <div className="py-1 px-3 rounded-full bg-purple/70 text-white text-sm">
+      New
+    </div>
+  )
+}
 
 const Account = () => {
   const { user } = UserAuth()
@@ -22,19 +30,46 @@ const Account = () => {
       <header className="sm:hidden">
         <Search />
       </header>
-      <div className="sm:p-4 flex flex-col gap-6">
+      <div className="sm:p-4 flex flex-col">
         {loadingMovies ? (
           <Loader />
         ) : (
           <>
-            <Rankings
-              rankings={rankings}
-              removeRanking={removeRanking}
-            />
-            <Favorites
-              favorites={favorites}
-              removeFromFavorites={removeFromFavorites}
-            />
+            <div className="flex flex-col gap-1 mb-8">
+              <h1 className="text-2xl">Hello, {user ? user.displayName.split(' ')[0] : ''} 👋</h1>
+
+              <p className="text-grayth text-sm">{user.email}</p>
+            </div>
+
+            <h1 className="text-lg mb-4">My movies</h1>
+            <section className="flex max-sm:flex-col justify-center m-auto items-center gap-2 w-full font-sans font-medium text-slate-300">
+              
+              <Link href='/rankings' className="rounded-lg w-full bg-[#07121f] py-6 px-4 hover:bg-[#091829] transition duration-200 ease-in">
+                <div className="flex items-center justify-between">
+                  <h2>Rankings</h2>
+                  {rankings.length} saved
+                  {/* <FaRegFolder /> */}
+                </div>
+              </Link>
+
+              <Link href='/favorites' className="rounded-lg w-full bg-[#07121f] py-6 px-4 hover:bg-[#091829] transition duration-200 ease-in">
+                <div className="flex items-center justify-between">
+                  <h2>Favorites</h2>
+                  {favorites.length ? <p>{favorites.length} saved</p> : <FaRegFolder />}
+                </div>
+              </Link>
+
+              <Link href='/saved' className="rounded-lg w-full bg-[#07121f] py-6 px-4 hover:bg-[#091829] transition duration-200 ease-in">
+                <div className="flex items-center justify-between relative">
+                  <div className="absolute right-5 bottom-8">
+                    <NewFeature />
+                  </div>
+                  <h2>Folders</h2>
+                  <FaRegFolder />
+                </div>
+              </Link>
+
+            </section>
           </>
         )}
       </div>
