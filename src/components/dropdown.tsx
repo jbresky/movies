2eb59a304614efccba23ba4236d16483076690b7
jbresky <joaquin_bresky@hotmail.com>
2024-01-27 15:13:17 -0300

    shadcn dropdown

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,95 +1,80 @@
-'use client'
-
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { SlArrowRight } from "react-icons/sl";
+import { FaCircleUser } from "react-icons/fa6";
+import { IoMdMenu } from "react-icons/io";
 
-import {
-    Drawer,
-    DrawerContent,
-    DrawerTrigger,
-} from "@/components/ui/drawer"
+interface IDropdown {
+    user: any,
+    handleLogout: () => void
+}
 
-import { UserAuth } from "@/context/auth-context";
-import useLoginModal from "@/hooks/use-login-modal";
-import useRegisterModal from "@/hooks/use-register-modal";
+const Dropdown = ({ user, handleLogout }: IDropdown) => {
 
-const Dropdown = () => {
-    const { user, logOut } = UserAuth()
-    const router = useRouter()
-
-    const handleLogout = () => {
-        router.push('/')
-        setTimeout(async () => {
-            await logOut()
-        }, 300)
-    }
-
-    const loginModal = useLoginModal()
-    const registerModal = useRegisterModal()
     return (
-        <Drawer>
-            <DrawerTrigger>
-                {user && user.photoURL ? (
-                    <Image
-                        className='rounded-full sm:hidden'
-                        width={40}
-                        height={40}
-                        alt={user.email}
-                        src={user.photoURL}
-                    />
-                ) : (
-                    <RxHamburgerMenu className='sm:hidden text-xl' />
-                )}
-            </DrawerTrigger>
-            <DrawerContent className="bg-[#0e0e0e] p-4 flex flex-col border-none text-sm gap-4">
-                {user && user.email ? (
-                    <>
-                        <Link href='/account' className="flex items-center gap-2 justify-between text-neutral-400">
-                            <span>
-                                Profile
-                            </span>
-                            <span className="text-[11px]">
-                                <SlArrowRight />
-                            </span>
-                        </Link>
-                        <Link href='/create-ranking' className="flex items-center justify-between gap-2 text-neutral-400">
-                            <span>
-                                Create ranking
-                            </span>
-                            <span className="text-[11px]">
-                                <SlArrowRight />
-                            </span>
-                        </Link>
-                        <button className="flex items-center gap-2 justify-between text-[#d5322d]" onClick={handleLogout}>
-                            <span>
-                                Log out
-                            </span>
-                            <span className="text-[11px]">
-                                <SlArrowRight />
-                            </span>
-                        </button>
-                    </>
-                ) : (
-                    <>
-                        <div className="flex items-center justify-between gap-2 text-neutral-400">
-                            <button onClick={loginModal.onOpen}>Log in</button>
-                            <span className="text-[11px]">
-                                <SlArrowRight />
-                            </span>
-                        </div>
-                        <div className="flex items-center justify-between gap-2 text-neutral-400">
-                            <button onClick={registerModal.onOpen}>Sign up</button>
-                            <span className="text-[11px]">
-                                <SlArrowRight />
-                            </span>
-                        </div>
-                    </>
-                )}
-            </DrawerContent>
-        </Drawer>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <div className='flex items-center gap-3 border-[1px] cursor-pointer hover:opacity-80 transition duration-200 border-grayth rounded-full py-2 px-3'>
+                    <IoMdMenu className='ml-1 text-xl' />
+                    {user.photoURL && (
+                        <Image
+                            className='rounded-full'
+                            width={30}
+                            height={30}
+                            alt={user.email}
+                            src={user.photoURL}
+                        />
+                    )}
+                    {!user.photoURL && (
+                        <FaCircleUser
+                            className='rounded-full sm:hidden text-2xl bg-indigo-900'
+                        />
+                    )}
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+
+                <Link href='/account'>
+                    <DropdownMenuLabel>
+                        Profile
+                    </DropdownMenuLabel>
+                </Link>
+                <Link href='/create-ranking'>
+                    <DropdownMenuLabel>
+                        Create ranking
+                    </DropdownMenuLabel>
+                </Link>
+
+                <DropdownMenuSeparator className="bg-grayth h-[0.5px]" />
+
+                <Link href='/favorites'>
+                    <DropdownMenuItem className="cursor-pointer">
+                        Favorites
+                    </DropdownMenuItem>
+                </Link>
+                <Link href='/rankings'>
+                    <DropdownMenuItem className="cursor-pointer">
+                        Rankings
+                    </DropdownMenuItem>
+                </Link>
+                <Link href='/folders'>
+                    <DropdownMenuItem className="cursor-pointer">
+                        Folders
+                    </DropdownMenuItem>
+                </Link>
+                <DropdownMenuItem>
+                    <button onClick={handleLogout} className="cursor-pointer">Log out</button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+
     );
 }
 
