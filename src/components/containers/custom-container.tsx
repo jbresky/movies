8229d9ecbc75ebded4b4b058d568bd7e0b9aa440
jbresky <joaquin_bresky@hotmail.com>
2024-01-28 20:25:50 -0300

    isScrollable prop & grid change

--- src/components/containers/custom-container.tsx
+++ src/components/containers/custom-container.tsx
@@ -11,24 +11,25 @@ interface ICustomContainer {
     addToRanking?: () => void,
     removeFromRanking?: () => void,
     ref?: any,
-    index?: number
+    index?: number,
+    isScrollable?: boolean
 }
 
-const CustomContainer = ({ item, classname, isRank, removeFromFavorites, addToRanking, ref, removeFromRanking, index }: ICustomContainer) => {
+const CustomContainer = ({ item, classname, isRank, removeFromFavorites, addToRanking, ref, removeFromRanking, index, isScrollable }: ICustomContainer) => {
     return (
         <>
-            <div className="w-[170px] md:w-[200px] lg:w-[250px] xl:w-[300px] flex flex-col gap-2 rounded-md overflow-hidden whitespace-nowrap">
+            <div className={`flex flex-col gap-3 ${isScrollable ? 'w-[170px]' : 'w-full'} w-[170px] sm:w-full rounded-md overflow-hidden whitespace-nowrap`}>
                 <div
                     ref={ref}
                     onClick={addToRanking}
-                    className="relative border-[1px] hover:shadow-neutral-800/50 shadow-lg border-transparent transition duration-800 cursor-pointer hover:border-grayth rounded-md">
+                    className="relative border-[1px] w-full hover:shadow-neutral-800/50 shadow-lg border-transparent transition duration-800 cursor-pointer hover:border-grayth rounded-md">
                     <Image
                         src={`https://image.tmdb.org/t/p/original/${item.img}`}
                         alt={item.title}
                         width={300}
                         height={375}
                         onLoad={(e: any) => e.target.classList.remove("opacity-0")}
-                        className="rounded-md opacity-0 transition-opacity duration-2000"
+                        className="rounded-md opacity-0 transition-opacity overflow-hidden duration-2000"
                     />
                     {
                         isRank !== null && (
@@ -44,7 +45,7 @@ const CustomContainer = ({ item, classname, isRank, removeFromFavorites, addToRa
                         )
                     }
                 </div>
-                <p className="leading-5 text-grayth text-sm">{item.title}</p>
+                <p className="leading-5 font-medium text-sm">{item.title}</p>
             </div>
         </>
     )
