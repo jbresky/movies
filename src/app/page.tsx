e8583e116da9ad526d0a49f59e316cf5d94d6165
jbresky <joaquin_bresky@hotmail.com>
2024-02-03 20:52:53 -0300

    cleaning home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { Toaster } from 'sonner'
-import CardMovie from "@/components/containers/card-movie"
-import MovieSkeleton from "@/components/containers/movie-skeleton"
 import { getTitle, getTopMovies } from "@/services/movies"
 import SearchM from "@/components/search-movies"
+import Movies from '@/components/movies'
 
 export default async function Home({ searchParams }: { searchParams: { title: string } }) {
 
@@ -30,27 +29,7 @@ export default async function Home({ searchParams }: { searchParams: { title: st
           {!searchParams.title ? <h1 className="text-xl font-medium font-sans">Top movies</h1> : null}
         </div>
 
-        {
-          movies.length > 0
-            ? (
-              <div className='max-2xsm:flex flex-col items-center justify-center grid grid-cols-2 s:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 py-4 justify-items-center'>
-                {movies.map((item: Movie) => (
-                  !movies
-                    ? (
-                      <MovieSkeleton key={item.id} item={item} />
-                    )
-                    : (
-                      <CardMovie
-                        key={item.id}
-                        item={item}
-                      />
-                    )
-                ))}
-              </div>
-            ) : (
-              <p className='pl-4 mt-4 text-neutral-300'>No matching movies found 😓</p>
-            )
-        }
+        <Movies movies={movies} />
       </main>
     </>
   )
