cbe95af85532f271e277ae71eb31e4125650668c
jbresky <joaquin_bresky@hotmail.com>
2024-02-03 21:16:22 -0300

    cleaning create-ranking page

---
+++ src/app/create-ranking/components/movie-results.tsx
@@ -0,0 +1,33 @@
+import CustomContainer from "@/components/containers/custom-container";
+
+interface IMovieResults {
+    movies: Movie[],
+    movieSearched: string,
+    addToRanking: (item: Movie) => void
+}
+
+const MovieResults = ({ movies, movieSearched, addToRanking }: IMovieResults) => {
+    return (
+        <section className='mt-2'>
+            {movies.length > 1 && movieSearched && <p className='mb-2'> Results for: <span className='text-indigo-400 font-semibold font-sans pl-1'>{movieSearched}</span></p>}
+            {movies.length < 1 && movieSearched && <p>Couldn&apos;t find any movie with name: <span className='text-grayth font-semibold font-sans pl-1'>{movieSearched}</span></p>}
+            <div className="flex flex-col 2xsm:grid grid-cols-2 s:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+
+                {movies.map((item: Movie) => (
+                    <div key={item.id} className="flex flex-col items-center">
+                        {item.img && (
+                            <CustomContainer
+                                item={item}
+                                isRank={null}
+                                addToRanking={() => addToRanking(item)}
+                            />
+                        )
+                        }
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default MovieResults;
--- src/app/create-ranking/page.tsx
+++ src/app/create-ranking/page.tsx
@@ -3,40 +3,24 @@
 import Loader from '@/components/loader'
 import useMovies from '@/hooks/use-movies'
 import useCreate from '@/hooks/use-create'
-import useSearch from '@/hooks/use-search'
 import { UserAuth } from '@/context/auth-context'
 import { redirect } from 'next/navigation'
 import { Toaster } from 'sonner'
-import { ChangeEvent, FormEvent, useState } from 'react'
 import SearchM from '@/components/search-movies'
 import Selected from './components/selected'
-import CustomContainer from '@/components/containers/custom-container'
+import MovieResults from './components/movie-results'
 
 const CreateRanking = () => {
-    const [movieSearched, setMovieSearched] = useState('')
-
-    const { title, setTitle } = useSearch()
-
-    const { movies, getOneMovie, loading } = useMovies()
 
     const { user } = UserAuth()
-
+    
     if (!user) {
         redirect('/')
     }
-
-    const changeSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.target.value)
-    }
-
-    const submitSearch = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        getOneMovie({ title })
-        setMovieSearched(title)
-    }
+    
+    const { loading, title, submitSearch, movies, changeSearch, movieSearched } = useMovies()
 
     const createRankingProps = useCreate()
-
     const { selectedMovies, addToRanking, removeFromRanking } = createRankingProps
 
     return (
@@ -62,25 +46,11 @@ const CreateRanking = () => {
                     loading ? (
                         <Loader />
                     ) : (
-                        <section className='mt-2'>
-                            {movies.length > 1 && movieSearched && <p className='mb-2'> Results for: <span className='text-indigo-400 font-semibold font-sans pl-1'>{movieSearched}</span></p>}
-                            {movies.length < 1 && movieSearched && <p>Couldn&apos;t find any movie with name: <span className='text-grayth font-semibold font-sans pl-1'>{movieSearched}</span></p>}
-                            <div className="flex flex-col 2xsm:grid grid-cols-2 s:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-
-                                {movies.map((item: Movie) => (
-                                    <div key={item.id} className="flex flex-col items-center">
-                                        {item.img && (
-                                            <CustomContainer
-                                                item={item}
-                                                isRank={null}
-                                                addToRanking={() => addToRanking({ item })}
-                                            />
-                                        )
-                                        }
-                                    </div>
-                                ))}
-                            </div>
-                        </section>
+                        <MovieResults
+                            movies={movies}
+                            movieSearched={movieSearched}
+                            addToRanking={addToRanking}
+                        />
                     )}
             </main>
         </>
