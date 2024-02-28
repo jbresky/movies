1ac1e4d32d4941202c39228f1be128494cfcf463
jbresky <joaquin_bresky@hotmail.com>
2024-02-28 09:38:19 +0100

    base_url

--- src/components/containers/card-movie.tsx
+++ src/components/containers/card-movie.tsx
@@ -4,10 +4,12 @@ import { headers } from "next/headers"
 import Link from "next/link";
 import RenderFavoriteIcon from "../render-favorite-icon";
 
+const BASE_URL = process.env.NODE_ENV == 'production' ? process.env.VERCEL_URL! : "http://localhost:3000"
+
 const CardMovie = ({ item }: { item: Movie }) => {
     const headersList = headers()
     const pathname = headersList.get("x-pathname")
-    const url = new URL(pathname ? pathname : "/", "/")
+    const url = new URL(pathname ? pathname : "/", BASE_URL)
     url.searchParams.set("modal", "true")
     url.searchParams.set("id", item.id.toString())
 
@@ -15,9 +17,10 @@ const CardMovie = ({ item }: { item: Movie }) => {
         <div className="w-[80%] 2xsm:w-full flex flex-col gap-2 text-ellipsis overflow-hidden whitespace-nowrap pb-2">
             {item.img !== null && (
                 <>
-                    <Link href={url.toString()} scroll={false}>
-                        <div
-                            className="relative border-[1px] hover:shadow-neutral-800/50 shadow-lg border-transparent transition duration-800 cursor-pointer hover:border-grayth rounded-md">
+                    <div
+                        className="relative border-[1px] hover:shadow-neutral-800/50 shadow-lg border-transparent transition duration-800 cursor-pointer hover:border-grayth rounded-md">
+                        <RenderFavoriteIcon item={item} />
+                        <Link href={url.toString()} scroll={false}>
                             <Image
                                 src={`https://image.tmdb.org/t/p/original/${item.img}`}
                                 alt={item.title}
@@ -27,10 +30,9 @@ const CardMovie = ({ item }: { item: Movie }) => {
                                 loading="lazy"
                             />
 
-                            <RenderFavoriteIcon item={item} />
-                        </div>
-                        <p className="leading-5 text-sm text-grayth">{item.title}</p>
-                    </Link>
+                        </Link>
+                    </div>
+                    <p className="leading-5 text-sm text-grayth">{item.title}</p>
                 </>
             )}
         </div>
